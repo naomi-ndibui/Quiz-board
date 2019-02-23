2291d1ed33a06e9664f8b4aaa2b87c5f7952ce40
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-02-23 08:04:56 +0300

    corrected spelling mistake

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,28 @@ $(document).ready(function(){
       var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
       var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
       var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+
+      if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
+       $('#questionsIncomplete').text('Please complete the questions before submitting');
+       $('#questionsIncomplete').fadeOut(10000);
+     } else {
+              if (answerOne === answers[0]) {
+         score += pointPerCorrect;
+       }
+              if (answerTwo === answers[1]) {
+         score += pointPerCorrect;
+       }
+              if (answerThree === answers[2]) {
+         score += pointPerCorrect;
+       }
+
+
+             $("input[type=radio][name=questionOneChoice]:checked").prop('checked', false);
+             $("input[type=radio][name=questionTwoChoice]:checked").prop('checked', false);
+             $("input[type=radio][name=questionThreeChoice]:checked").prop('checked', false);
+             $('#questionsIncomplete').text('');
+             $('#result').text(percentage(score));
+     }
+               event.preventDefault();
+      });
+     });
