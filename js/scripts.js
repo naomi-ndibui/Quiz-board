036647c98cc85b650a53c47666c346461f8edf9b
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-02-23 08:21:47 +0300

    Adjusted the scripts file too accomodate the functions on question four

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-var answers = ["Storing numbers,dates or other values","Accept parameters","script tag"];
-var pointPerCorrect = 33.3;
+var answers = ["Storing numbers,dates or other values", "Accept parameters", "script tag", "Hyper Text Markup Language"];
+var pointPerCorrect = 25;
 
 
 function percentage(score) {
-return "Your score is " + parseInt((score / 66.6) * 100) + "%";
+return "Your score is " + parseInt((score / 75) * 100) + "%";
 }
 
 $(document).ready(function(){
@@ -15,8 +15,9 @@ $(document).ready(function(){
       var answerOne = ($("input[type=radio][name=questionOneAnswer]:checked").val());
       var answerTwo = ($("input[type=radio][name=questionTwoAnswer]:checked").val());
       var answerThree = ($("input[type=radio][name=questionThreeAnswer]:checked").val());
+      var answerFour = ($("input[type=radio][name=questionFourAnswer]:checked").val());
 
-      if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
+      if (answerOne === undefined || answerTwo === undefined || answerThree === undefined || answerFour === undefined) {
        $('#questionsIncomplete').text('Please complete the questions before submitting');
        $('#questionsIncomplete').fadeOut(10000);
      } else {
@@ -29,11 +30,15 @@ $(document).ready(function(){
               if (answerThree === answers[2]) {
          score += pointPerCorrect;
        }
+             if (answerFour === answers[3]) {
+         score += pointPerCorrect;
+       }
 
 
              $("input[type=radio][name=questionOneChoice]:checked").prop('checked', false);
              $("input[type=radio][name=questionTwoChoice]:checked").prop('checked', false);
              $("input[type=radio][name=questionThreeChoice]:checked").prop('checked', false);
+             $("input[type=radio][name=questionFourChoice]:checked").prop('checked', false);
              $('#questionsIncomplete').text('');
              $('#result').text(percentage(score));
      }
